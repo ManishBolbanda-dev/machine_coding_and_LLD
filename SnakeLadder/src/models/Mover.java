cb04a8268023ac50377b5b5ffcb21df5710f058c
ManishBolbanda-dev <manishmb122333@gmail.com>
2025-03-09 03:18:09 +0530

    snake ladder code for machcoding

---
+++ SnakeLadder/src/models/Mover.java
@@ -0,0 +1,26 @@
+package models;
+
+public class Mover {
+	private int from;
+	private int to;
+	
+	public Mover(int from, int to) {
+		super();
+		this.from = from;
+		this.to = to;
+	}
+	public int getFrom() {
+		return from;
+	}
+	public void setFrom(int from) {
+		this.from = from;
+	}
+	public int getTo() {
+		return to;
+	}
+	public void setTo(int to) {
+		this.to = to;
+	}
+	
+	
+}
